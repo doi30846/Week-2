c058dcd109a56429472b8f15e57a4b13bb978b40
doi30846 <doi30846@gmail.com>
2015-08-28 17:00:19 +0700

    program

--- Lab201/Program.cs
+++ Lab201/Program.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace Lab2
+namespace Lab201
 {
 	class MainClass
 	{
@@ -27,6 +27,7 @@ namespace Lab2
 			for (int i = 4; i < stds.Length; i++)
 				Console.WriteLine (stds [i]);
 
+            Console.ReadLine();
 		}
 	}
 }
